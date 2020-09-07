2fdecfde50652b0b04813ccc0b51dbc666557990
Dylan-Eckhart <deckhart@roc-dev.com>
2020-09-07 15:40:12 +0200

    Push

---
+++ src/HO4/Opdracht6.java
@@ -0,0 +1,15 @@
+package HO4;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht4 extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.white);
+        g.setColor(Color.gray);
+        g.fillArc();
+    }
