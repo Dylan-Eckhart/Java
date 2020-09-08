954afd7d9b76ae2577b4d5d52db27bdb9c4a437f
Dylan-Eckhart <deckhart@roc-dev.com>
2020-09-08 09:05:56 +0200

    Push

--- src/HO4/Opdracht6.java
+++ src/HO4/Opdracht6.java
@@ -3,7 +3,7 @@ package HO4;
 import java.awt.*;
 import java.applet.*;
 
-public class Opdracht4 extends Applet {
+public class Opdracht6 extends Applet {
 
     public void init() {
     }
@@ -11,12 +11,14 @@ public class Opdracht4 extends Applet {
     public void paint(Graphics g) {
         setBackground(Color.white);
         g.setColor(Color.gray);
-        g.fillArc(200,175,40,30,0,150);
+        g.fillArc(200,175,40,30,0,180);
         g.fillRect(200,190,40,50);
-        g.fillArc(200,225,40,30,100,100);
+        g.fillArc(200,225,40,30,180,180);
         g.setColor(Color.red);
         g.fillArc(210,180,20,20,0,360);
         g.setColor(Color.ORANGE);
-        g.fillArc(210,180,20,20,0,360);
+        g.fillArc(210,205,20,20,0,360);
+        g.setColor(Color.GREEN);
+        g.fillArc(210,230,20,20,0,360);
     }
 }
