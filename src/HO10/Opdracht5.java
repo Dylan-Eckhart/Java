e4ba5e6c0bf9bba599b740aac6fd883780aa14bb
Dylan-Eckhart <deckhart@roc-dev.com>
2020-09-14 11:02:29 +0200

    Push

---
+++ src/HO10/Opdracht5.java
@@ -0,0 +1,56 @@
+package HO10;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Opdracht5 extends Applet {
+
+    //Declaratie
+    Label Label;
+    TextField Tekstvak;
+    double Gemiddelde;
+    int AantalCijfers;
+    boolean Voldoende;
+    Button Submit;
+    String Slaging;
+
+    public void init() {
+        Label = new Label("Voer een cijfer in");
+        Tekstvak =  new TextField("",5);
+        Submit = new Button("Submit");
+        Submit.addActionListener(new knoplistenerSubmit());
+
+        AantalCijfers = 0;
+        Gemiddelde = 0.0;
+        add(Tekstvak);
+        add(Submit);
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("Je gemiddelde cijfer: " + Gemiddelde,50,50);
+        g.drawString("Is het cijfer voldoende? " + Voldoende,50,70);
+        g.drawString("Aantal cijfers: " + AantalCijfers,50,90);
+    }
+
+    private class knoplistenerSubmit implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            Double Cijfer =  Double.parseDouble(Tekstvak.getText());
+            if (Cijfer >= 5.5) {
+                Voldoende = true;
+            } else {
+                Voldoende = false;
+            }
+            Gemiddelde = ((Gemiddelde * AantalCijfers) + Cijfer) / (AantalCijfers +1);
+            System.out.println(Gemiddelde);
+            Gemiddelde =  Gemiddelde * 10;
+            int Gemiddelde_ = (int) Gemiddelde;
+            Gemiddelde =  Gemiddelde_;
+            Gemiddelde = Gemiddelde / 10;
+            AantalCijfers++;
+            repaint();
+
+        }
+    }
+}
