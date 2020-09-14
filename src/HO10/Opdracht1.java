8a77490b6d2660e6b317090f02362e33df073928
Dylan-Eckhart <deckhart@roc-dev.com>
2020-09-14 09:01:20 +0200

    Push

---
+++ src/HO10/Opdracht1.java
@@ -0,0 +1,45 @@
+package HO10;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Opdracht1 extends Applet {
+
+    //Declaratie
+    TextField Tekstvak;
+    double Tekst;
+    double Getal;
+    Label Label;
+    String S;
+
+    public void init() {
+
+        //Initalisatie
+        Getal = 7;
+        Tekstvak = new TextField("",10);
+        Tekstvak.addActionListener(new Tekstvaklistener());
+        Label = new Label("Voer een getal in en druk op enter!");
+
+        add(Label);
+        add(Tekstvak);
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("Het getal dat u heeft ingevoerd is: " + Getal,20,50);
+    }
+
+    private class Tekstvaklistener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            S = Tekstvak.getText();
+            Tekst = Double.parseDouble(S);
+            if (Tekst > Getal) {
+                Getal = Tekst;
+            }
+            repaint();
+        }
+    }
+
+}
