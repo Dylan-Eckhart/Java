98d4ef4a755afb77d8111d3dd8bb80e2e17656f8
Dylan-Eckhart <deckhart@roc-dev.com>
2020-09-15 09:13:44 +0200

    Push

--- src/HO11/Opdracht1.java
+++ src/HO11/Opdracht1.java
@@ -7,9 +7,7 @@ public class Opdracht1 extends Applet {
     public void init() {}
 
     public void paint(Graphics g) {
-        //loop die 10 lijnen elk op andere hoogte zet
         for(int i=1; i<=10; i++) {
-            //hoogte word bepaald door de i
             g.drawLine(10, i*10,210,i*10);
         }
     }
