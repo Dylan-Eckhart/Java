aeb220b9fb26968a644df51c6ad1e88db62c0a22
Dylan-Eckhart <deckhart@roc-dev.com>
2020-09-16 11:03:33 +0200

    Push

--- src/HO12/Opdracht1.java
+++ src/HO12/Opdracht1.java
@@ -35,7 +35,7 @@ public class Opdracht1 extends Applet {
         I = 6.8;
         J = 8;
 
-        Gemiddelde = new double[10];
+        Gemiddelde = new double[11];
         Uitkomst = (A + B + C + D + E + F + G + H + I + J) / 10;
 
 
@@ -49,6 +49,7 @@ public class Opdracht1 extends Applet {
         Gemiddelde[7] = H;
         Gemiddelde[8] = I;
         Gemiddelde[9] = J;
+        Gemiddelde[10] = Uitkomst;
 
         for (int teller = 0; teller < Gemiddelde.length; teller++) {
         }
@@ -56,8 +57,8 @@ public class Opdracht1 extends Applet {
 
     public void paint(Graphics g) {
         for (int teller = 0; teller < Gemiddelde.length; teller++) {
-            g.drawString("" + Gemiddelde[teller],50,20 * teller + 20);
+            g.drawString("" + Gemiddelde[teller],105,20 * teller + 20);
         }
-        g.drawString("Gemiddelde",20,220);
+        g.drawString("Gemiddelde: ",20,220);
     }
 }
