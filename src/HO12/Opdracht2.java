ff8984cebdf2308b2bd4ea340c7c5eab6c15511c
Dylan-Eckhart <deckhart@roc-dev.com>
2020-09-16 11:15:38 +0200

    Push

---
+++ src/HO12/Opdracht2.java
@@ -0,0 +1,20 @@
+package HO12;
+
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Opdracht2 extends Applet {
+
+    //Declaratie
+    Button Buttons[] = new Button[25];
+
+    public void init() {
+
+        //Initalisatie
+        for (int i = 0; i != 25; i++) {
+            Buttons[i] = new Button("Knop " + String.valueOf(i + 1));
+            add(Buttons[i]);
+        }
+    }
+}
