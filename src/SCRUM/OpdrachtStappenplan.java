f8ebffc914923f33e7294f23c64f76191b72bdfe
Dylan-Eckhart <deckhart@roc-dev.com>
2020-09-30 15:39:33 +0200

    Push

--- src/SCRUM/OpdrachtStappenplan.java
+++ src/SCRUM/OpdrachtStappenplan.java
@@ -1,5 +1,7 @@
 package SCRUM;
 
+import apple.laf.JRSUIConstants;
+
 import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -15,7 +17,12 @@ public class OpdrachtStappenplan extends Applet {
         //Initalisatie
         getal = 13;
          if (getal >= 0 && getal == (int)getal) {
+             for (int i = 1; i < getal; i++) {
+                 if (i % 2 == 0) {
+                     System.out.println(i);
+                 }
 
+             }
          }
 
     }
